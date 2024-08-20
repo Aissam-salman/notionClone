1b3b152f201ae7636a4ecff23cd6e1ccc0ecdffd
Aissam-salman <nasawi.lmj@gmail.com>
2024-08-20 12:25:01 +0200

    feat: update title of document

--- app/(main)/_components/navbar.tsx
+++ app/(main)/_components/navbar.tsx
@@ -1,6 +1,12 @@
 "use client"
 
 import React from "react";
+import {useParams} from "next/navigation";
+import {useQuery} from "convex/react";
+import {api} from "@/convex/_generated/api";
+import {Id} from "@/convex/_generated/dataModel";
+import {MenuIcon} from "lucide-react";
+import {Title} from "@/app/(main)/_components/title";
 
 interface NavbarProps {
   isCollapsed?: boolean;
@@ -8,7 +14,37 @@ interface NavbarProps {
 }
 
 const Navbar = ({ isCollapsed, onResetWidth }: NavbarProps) => {
-  return <div>navbar!</div>;
+
+  const params = useParams();
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId as Id<"documents">,
+  });
+
+  if(document === undefined) {
+    return (
+        <nav className="bg-background dark:bg-neutral-800 px-3 py-2 w-full flex items-center">
+            <Title.Skeleton />
+        </nav>
+    )
+  }
+  if(document === null) return null;
+
+  return (
+      <>
+        <nav className="bg-background dark:bg-neutral-800 px-3 py-2 w-full flex items-center gap-x-4">
+            {isCollapsed && (
+                <MenuIcon
+                    role="button"
+                    onClick={onResetWidth}
+                    className="h-6 w-6 text-muted-foreground"
+                />
+            )}
+            <div className="flex items-center justify-between w-full">
+                <Title initialData={document} />
+            </div>
+        </nav>
+      </>
+  )
 };
 
 export default Navbar;
--- convex/documents.ts
+++ convex/documents.ts
@@ -2,6 +2,7 @@ import { v } from "convex/values";
 
 import { mutation, query } from "@/convex/_generated/server";
 import {Doc, Id} from "@/convex/_generated/dataModel";
+import arg from "arg";
 
 export const archive = mutation({
   args: {
@@ -215,6 +216,35 @@ export const getById = query({
 
     if(document.userId !== userId) throw new Error("Unauthorized");
 
+    return document;
+  }
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean())
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+    if(!identity) throw new Error("Not authenticated");
+    const userId = identity.subject;
+    const {id,...rest} = args;
+
+    const existingDocument = await ctx.db.get(args.id);
+    if(!existingDocument) throw new Error("Not found");
+
+    if(existingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const updateFields = Object.fromEntries(
+      Object.entries(rest).filter(([_, value]) => value !== undefined)
+    );
+
+    const document = await ctx.db.patch(id, updateFields);
     return document;
   }
 })
